5814d62d41505764ab99ca06efa93cd70eb7e235
Oleksandr Bystrikov <coder@yakviter.com.ua>
2023-11-08 20:04:41 +0100

    refactor: add site to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from 'astro/config';
-import vue from "@astrojs/vue";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import vue from '@astrojs/vue'
+import tailwind from '@astrojs/tailwind'
 
-import netlify from "@astrojs/netlify/functions";
+import netlify from '@astrojs/netlify/functions'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), tailwind()],
-  output: "server",
-  adapter: netlify()
-});
+  output: 'server',
+  adapter: netlify(),
+  site: 'https://ice-and-fire.softbeehive.com',
+})
